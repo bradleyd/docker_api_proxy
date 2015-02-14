0ed88012e5b2a69f5f19ca67adb87dd09be646d1
Bradley Smith <bradleydsmith@gmail.com>
2015-02-14 09:30:27 -0700

    setup basic OTP

--- lib/docker_api_proxy.ex
+++ lib/docker_api_proxy.ex
@@ -1,45 +1,20 @@
+require Logger
+
 defmodule DockerApiProxy do
-  import Plug.Conn
-  use Plug.Router
-  plug :match
-  plug :dispatch
-    
-  def init(options) do
-    options
+  use Application 
+
+  def start(_type, _args) do
+    opts  = [port: 4000, compress: true, linger: {true, 10}]
+
+    #if port = System.get_env("PORT") do
+      #opts = Keyword.put(opts, :port, String.to_integer(port))
+    #end
+
+    #config(opts[:port])
+    #File.mkdir_p!("tmp")
+
+    Logger.info "Starting Cowboy on port #{opts[:port]}"
+    Plug.Adapters.Cowboy.http(DockerApiProxy.Server, [], opts)
+    DockerApiProxy.Supervisor.start_link
   end
-
-  get "/hello" do
-    send_resp(conn, 200, "world")
-  end
-
-  get "/containers" do
-    hosts = ["192.168.4.4:14443", "127.0.0.1:14443"]
-    res = Enum.flat_map(hosts, fn(x) -> 
-    Application.put_env(:erldocker, :docker_http, x)
-    {:ok, result } = :docker_container.containers
-    result
-    end)
-
-    {:ok, enc } = JSON.encode(res)
-    send_resp(conn, 200, enc)
-  end
-
-  get "/container/:id" do
-    hosts = ["192.168.4.4:14443", "127.0.0.1:14443"]
-    res = Enum.flat_map(hosts, fn(x) -> 
-      Application.put_env(:erldocker, :docker_http, x)
-      case :docker_container.container(id) do
-        {:ok, result } -> result
-        _ -> []
-      end
-    end)
-
-    {:ok, enc } = JSON.encode(res)
-    send_resp(conn, 200, enc)
-  end
-
-  match _ do
-    send_resp(conn, 404, "oops")
-  end
-
 end

a8304bb758a48268e9a91cd6318c529d13441daf
Bradley Smith <bradleydsmith@gmail.com>
2015-02-21 21:25:57 -0700

    add images test

---
+++ test/image_test.exs
@@ -0,0 +1,68 @@
+defmodule DockerApiProxy.ImageTest do
+  use ExUnit.Case, async: true
+  use Plug.Test
+
+  @opts DockerApiProxy.Router.init([])
+
+  @host "192.168.4.4:14443"
+  @cid "971f52624eb3"
+
+  setup do
+    {:ok, pid} = DockerApiProxy.Supervisor.start_link
+    {:ok, pid: pid}
+  end
+
+  test "returns all images", %{pid: pid} do
+    body = %{name: @host}
+    conn1 = conn(:post, "/hosts", JSON.encode!(body), headers: [{"content-type", "application/json"}])
+    conn1 = DockerApiProxy.Router.call(conn1, [])
+
+
+    conn = conn(:get, "/images")
+
+    conn = DockerApiProxy.Router.call(conn, [])
+
+    decoded = JSON.decode(conn.resp_body)
+    # Assert the response and status
+    assert conn.state == :sent
+    assert conn.status == 200
+    assert {:ok, _} = decoded
+  end 
+
+  test "returns a specific image" do
+    body = %{name: @host}
+    conn1 = conn(:post, "/hosts", JSON.encode!(body), headers: [{"content-type", "application/json"}])
+    conn1 = DockerApiProxy.Router.call(conn1, [])
+
+
+    conn = conn(:get, "/image/#{@cid}")
+
+    conn = DockerApiProxy.Router.call(conn, [])
+
+    decoded = JSON.decode(conn.resp_body)
+    IO.inspect decoded
+    # Assert the response and status
+    assert conn.state == :sent
+    assert conn.status == 200
+    assert {:ok, _} = decoded
+  end 
+
+  test "creates a image" do
+    body = %{name: @host}
+    conn1 = conn(:post, "/hosts", JSON.encode!(body), headers: [{"content-type", "application/json"}])
+    conn1 = DockerApiProxy.Router.call(conn1, [])
+
+    body  = %{ "Image": "redis",
+               "HostName": "foobar",
+               "ExposedPorts": %{ "22/tcp": %{}, "6379/tcp": %{} },
+               "PortBindings": %{ "22/tcp": [%{ "HostIp": "192.168.4.4" }], 
+                                  "6379/tcp": [%{ "HostIp": "192.168.4.4" }]}}
+
+    conn = conn(:post, "/images", JSON.encode!(body), headers: [{"content-type", "application/json"}])
+    conn = DockerApiProxy.Router.call(conn, [])
+    {:ok, results } = JSON.decode(conn.resp_body)
+    IO.inspect results
+  end 
+
+
+end
